68ebabd403ca1a95aedf6443a16c0d7b1570eba4
hmoldenhauer <hmoldenhauer@users.noreply.github.com>
2019-02-07 14:20:54 +0100

    Carousel (#263)
    * first version of carousel added
    
    * changed width to 90% to have the same card-width
    
    * removed doubles
    
    * changed colors accordingly and moved indicator position
    
    * fine tuning
    
    * added randomization of carousel
    
    * changed interval time to 30s

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,3 +23,18 @@ if($('#pepNavContainer').hasClass('nav-landing')){
     };
   });
 }
+
+var $numberofSlides = $('.carousel-item').length;
+var $currentSlide = Math.floor((Math.random() * $numberofSlides));
+var $item = $('.carousel-item');
+
+$('.carousel-indicators li').each(function(){
+  var $slideValue = $(this).attr('data-slide-to');
+  if($currentSlide == $slideValue) {
+    $(this).addClass('active');
+    $item.eq($slideValue).addClass('active');
+  } else {
+    $(this).removeClass('active');
+    $item.eq($slideValue).removeClass('active');
+  }
+});

1d16d73fe9b60baafd79b903f40e87613b891937
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-01-05 21:21:58 +0100

    Update bootstrap to version 5.2.3, get it with bundler

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,35 +6,20 @@ window.onload = function () {
     let pepHeight = pepElem.offsetHeight;
     document.getElementById('content').style.marginTop = "-" + (pepHeight / 2) + "px";
   }
-};
 
+  navbar = document.getElementById("pepNavContainer");
+  window.addEventListener("scroll", () => {
+    console.log("scroll")
 
-if($('#pepNavContainer').hasClass('nav-landing')){
-  $(window).scroll(function (event) {
-    var pos = $(window).scrollTop();
-    var container = $('#pepNavContainer');
+    let pos = window.scrollY;
 
-    if(pos > 300 && container.hasClass('bg-faded')){
-      container.addClass('bg-dark');
-      container.removeClass('bg-faded');
-    } else if(pos <= 300 && container.hasClass('bg-dark')) {
-      container.removeClass('bg-dark');
-      container.addClass('bg-faded');
+    if(pos > 300 && navbar.classList.contains('bg-faded')){
+      navbar.classList.add('bg-dark');
+      navbar.classList.remove('bg-faded');
+    } else if(pos <= 300 && navbar.classList.contains('bg-dark')) {
+      navbar.classList.add('bg-faded');
+      navbar.classList.remove('bg-dark');
     };
   });
-}
 
-var $numberofSlides = $('.carousel-item').length;
-var $currentSlide = Math.floor((Math.random() * $numberofSlides));
-var $item = $('.carousel-item');
-
-$('.carousel-indicators li').each(function(){
-  var $slideValue = $(this).attr('data-slide-to');
-  if($currentSlide == $slideValue) {
-    $(this).addClass('active');
-    $item.eq($slideValue).addClass('active');
-  } else {
-    $(this).removeClass('active');
-    $item.eq($slideValue).removeClass('active');
-  }
-});
+};

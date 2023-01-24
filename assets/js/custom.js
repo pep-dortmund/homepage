b72f44d77f42a937063b8359e0c67f6eddf68dcf
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2023-01-24 18:02:28 +0100

    Make sure "special" navbar is only used on main page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,18 @@
+let navbar;
+
+function navBarScroll() {
+  let pos = window.scrollY;
+  if(pos > 300 && navbar.classList.contains('bg-faded')){
+    navbar.classList.add('bg-dark');
+    navbar.classList.remove('bg-faded');
+  } else if(pos <= 300 && navbar.classList.contains('bg-dark')) {
+    navbar.classList.add('bg-faded');
+    navbar.classList.remove('bg-dark');
+  }
+}
+
 window.onload = function () {
   let pepElem = document.getElementById('pep');
-
   if (pepElem !== null) {
     console.log("adjusting height");
     let pepHeight = pepElem.offsetHeight;
@@ -8,18 +20,7 @@ window.onload = function () {
   }
 
   navbar = document.getElementById("pepNavContainer");
-  window.addEventListener("scroll", () => {
-    console.log("scroll")
-
-    let pos = window.scrollY;
-
-    if(pos > 300 && navbar.classList.contains('bg-faded')){
-      navbar.classList.add('bg-dark');
-      navbar.classList.remove('bg-faded');
-    } else if(pos <= 300 && navbar.classList.contains('bg-dark')) {
-      navbar.classList.add('bg-faded');
-      navbar.classList.remove('bg-dark');
-    };
-  });
-
+  if (navbar.classList.contains("nav-landing")) {
+    window.addEventListener("scroll", navBarScroll);
+  }
 };
